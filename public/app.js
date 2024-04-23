7f17388cb3fbb3b8c19c3b2600d467dbc02c1db7
Erik Behrends <behrends@users.noreply.github.com>
2024-04-23 20:13:43 +0200

    use actual form data

--- public/app.js
+++ public/app.js
@@ -1,6 +1,8 @@
 document.getElementById('laden').addEventListener('click', async () => {
-    const response = await fetch('/api/chat');
-    const data = await response.json();
-    document.getElementById('daten').textContent = JSON.stringify(data.message);
+  const persona = document.getElementById('persona').value;
+  const topic = document.getElementById('thema').value;
+  const response = await fetch(`/api/chat?persona=${persona}&topic=${topic}`);
+  const data = await response.json();
+  document.getElementById('daten').textContent = JSON.stringify(data.message);
 });
 
--- server.js
+++ server.js
@@ -9,20 +9,25 @@ const app = express();
 
 app.use(express.static('public'));
 
-const pirate = 'Benutze Piratensprache und sprich, als wärst du ein Pirat von den hohen Meeren.'
-const yoda = 'In umgekehrter Satzanordnung du sprechen musst, wie Yoda.'
-const oracle = 'Benutze die geheimnisvolle, vage Sprache eines griechischen Orakels.'
+const personas = { 
+  pirate: 'Benutze Piratensprache und sprich, als wärst du ein Pirat von den hohen Meeren.',
+  yoda: 'In umgekehrter Satzanordnung du sprechen musst, wie Yoda.',
+  oracle: 'Benutze die geheimnisvolle, vage Sprache eines griechischen Orakels.',
+};
 
-const weather = 'Erfinde eine kurze Wettervorhersage für morgen (max. 30 Wörter).'
-const horoscope = 'Erfinde ein kurzes positives Horoskop (max. 30 Wörter).'
-const news = 'Erfinde eine kurze dramatische Nachrichtenmeldung (max. 30 Wörter).'
+const topics = {
+ weather: 'Erfinde eine kurze Wettervorhersage für morgen (max. 30 Wörter).',
+ horoscope: 'Erfinde ein kurzes positives Horoskop (max. 30 Wörter).',
+ news: 'Erfinde eine kurze kuriose Nachrichtenmeldung (max. 30 Wörter).',
+};
 
 app.get('/api/chat', async (req, res) => {
+  const { persona, topic } = req.query;
   const chatCompletion = await openai.chat.completions.create({
     messages: [
       {
         role: 'user',
-        content: `${oracle} ${horoscope}`,
+        content: `${personas[persona]} ${topics[topic]}`,
       },
     ],
     model: 'gpt-4-turbo',
