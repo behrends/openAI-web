89379bff384eae7114a4ef3a26452d0bdaa4b17e
Erik Behrends <behrends@users.noreply.github.com>
2024-04-23 20:02:09 +0200

    add yoda and greek oracle

--- server.js
+++ server.js
@@ -10,15 +10,19 @@ const app = express();
 app.use(express.static('public'));
 
 const pirate = 'Benutze Piratensprache und sprich, als wärst du ein Pirat von den hohen Meeren.'
+const yoda = 'In umgekehrter Satzanordnung du sprechen musst, wie Yoda.'
+const oracle = 'Benutze die geheimnisvolle, vage Sprache eines griechischen Orakels.'
 
 const weather = 'Erfinde eine kurze Wettervorhersage für morgen (max. 30 Wörter).'
+const horoscope = 'Erfinde ein kurzes positives Horoskop (max. 30 Wörter).'
+const news = 'Erfinde eine kurze dramatische Nachrichtenmeldung (max. 30 Wörter).'
 
 app.get('/api/chat', async (req, res) => {
   const chatCompletion = await openai.chat.completions.create({
     messages: [
       {
         role: 'user',
-        content: `${pirate} ${weather}`,
+        content: `${oracle} ${horoscope}`,
       },
     ],
     model: 'gpt-4-turbo',
